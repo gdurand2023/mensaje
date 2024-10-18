9e4f1a0a858e649d8edd964d9ce0133f61926a84
gdurand2023 <131704538+gdurand2023@users.noreply.github.com>
2024-10-18 10:48:42 -0500

    Add files via upload

---
+++ app01.js
@@ -0,0 +1,5 @@
+document.write("<h1>Bienvenido a la pagina de JavaScript</h1>");
+var nom = prompt("Ingrese su nombre");
+document.write(
+  "Hola " + nom + " es tu primer contacto con la programación en JavaScript"
+);
